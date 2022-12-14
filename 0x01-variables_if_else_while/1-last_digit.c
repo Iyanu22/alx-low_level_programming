6224fdf48d42ed1a1d51f1725a12384642c7af1f
Iyanu22 <olajideiyanu22@gmail.com>
2022-12-14 11:25:37 -0800

    if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,12 @@
 /* more headers goes there */
 
 
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */  
+/* Betty style doc for function main goes there */
 
 int main(void)
 
