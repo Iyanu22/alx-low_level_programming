48e27e0ea2311c37a741737cf28821215335ed42
Iyanu22 <olajideiyanu22@gmail.com>
2022-12-14 16:29:21 -0800

    capitalsmall

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,15 @@
-#include<stdio.h> 
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 
-int main(void) 
-
-{ 
+int main(void)
+{
 	char alpha;
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 	putchar(alpha);
