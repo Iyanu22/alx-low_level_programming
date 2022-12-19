d02dd8957041ed32aedbf333d232a1e2320623f5
Iyanu22 <olajideiyanu22@gmail.com>
2022-12-18 16:10:58 -0800

    digitts

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,15 +2,15 @@
 /**
  * main - prints all single digit numbers of base 10 starting from 0
  *
- * Return - Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	int i = 0;
+
 	for (i = 0; i < 10; i++)
 	printf("%d", i);
 
-	printf ("\n");
-	
+	printf("\n");
 	return (0);
 }
