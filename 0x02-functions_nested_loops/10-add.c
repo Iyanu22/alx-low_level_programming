5e7f7e20c9d020ef9b263e399e1ebf48ce26a924
Iyanu22 <olajideiyanu22@gmail.com>
2022-12-29 08:31:24 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,6 +3,8 @@
  * add - adds two integers and returns the result.
  * @num1: The first integer to be added
  * @num2: The second integer to be added.
+ *
+ * Return: the value of the integer.
  */
 int add(int num1, int num2)
 {
