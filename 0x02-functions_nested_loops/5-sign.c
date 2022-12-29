8ab6384dd25263c903a99039cfc2bb7f01c7d03b
Iyanu22 <olajideiyanu22@gmail.com>
2022-12-29 06:26:28 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
  * int print_sign -  prints the sign of a number.
  * @c: The character to be checked.
  * Return: 1 and prints + if n is greater than zero
- * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
+ * Return: 0 and prints 0 if n is zero
+ * Returns: -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -15,6 +15,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
+	_putchar('0');
 	return (0);
 	}
 	else
