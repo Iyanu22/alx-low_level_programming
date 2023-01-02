773fdcd5addb789a9921f7302fd2b5e75b347710
Iyanu22 <olajideiyanu22@gmail.com>
2023-01-02 04:09:27 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,10 +8,16 @@
 void print_diagonal(int n)
 {
 	int i;
-
+	
+	if (i <= 0)
+	_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
-	_putchar('\');
+	for (j = 0; j < i; j++)
+	{
+	_putchar(' ');
 	}
+	_putchar('\\');
 	_putchar('\n');
+	}
 }
