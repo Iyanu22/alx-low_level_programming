58062cc090a8673f4775ef32f65f082ed654f26b
Iyanu22 <olajideiyanu22@gmail.com>
2023-01-03 08:36:55 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * _strle - returns the length of a string.
- * @size: the first size.
+ * _strlen - returns the length of a string.
+ * @s: the first size.
+ *
  * Return: lngth of string.
  */
 int _strlen(char *s)
 {
 	int seize = 0;
+
 	for (; *s++;)
 	seize++;
-	return(seize);
+	return (seize);
 }
