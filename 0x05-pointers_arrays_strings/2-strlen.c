d582a5e5c04a93eb0b6613fea3dd15275a845727
Iyanu22 <olajideiyanu22@gmail.com>
2023-01-03 08:24:36 -0800

    int _strlen(char *s)

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strle - returns the length of a string.
+ * @size: the first size.
+ * Return: lngth of string.
+ */
+int _strlen(char *s)
+{
+	seize = 0;
+	while (*str++)
+	seize++:
+	return(seize);
+}
